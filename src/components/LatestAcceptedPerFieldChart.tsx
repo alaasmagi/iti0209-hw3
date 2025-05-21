7c412a2d3054169d721ccf9d3a8d94f41304c40f
Aleksander Laasmägi <allaas@taltech.ee>
2025-05-21 11:02:01 +0300

    Updated one chart

--- src/components/LatestAcceptedPerFieldChart.tsx
+++ src/components/LatestAcceptedPerFieldChart.tsx
@@ -6,78 +6,77 @@ export default function LatestAcceptedPerFieldChart() {
   const [data, setData] = useState<FieldData[]>([]);
 
   useEffect(() => {
-    fetch("/data/latestGraduatedPerField.json")
-      .then(res => res.json())
+    fetch("/data/latestAcceptedPerField.json")
+      .then((res) => res.json())
       .then(setData);
   }, []);
 
   const option = {
-  title: {
-    text: "Õppeaastal 2023/24 kõrgharidust alustanud tudengid \n valdkondade kaupa",
-    left: "center",
-    textStyle: {
-      color: "white"
-    }
-  },
-  tooltip: {
-    trigger: "axis",
-    axisPointer: {
-      type: "shadow"
-    }
-  },
-  grid: {
-    left: "1%",
-    right: "15%",
-    bottom: "0%",
-    containLabel: true
-  },
-  xAxis: {
-    type: "category",
-    data: data.map(d => d.field),
-    axisLabel: {
-      interval: 0,
-      rotate: 30,
-      color: "white",
-      formatter: function (value: string) {
-      return value.length > 10 ? value.slice(0, 10) + "…" : value;
-    }
-    },
-    name: "Valdkond",
-     nameTextStyle: {
-      color:"white"
-    }
-  },
-  yAxis: {
-    type: "value",
-    name: "Alustajate arv",
-     axisLabel: {
-      color: "white"
-    },
-    nameTextStyle: {
-      color:"white"
-    }
-  },
-  series: [
-    {
-      name: "Alustajate arv",
-      type: "bar",
-      data: data.map(d => d.rate),
-      itemStyle: {
-        color: '#5470C6'
+    title: {
+      text: "Õppeaastal 2023/24 kõrgharidust alustanud tudengid \n valdkondade kaupa",
+      left: "center",
+      textStyle: {
+        color: "white",
       },
-      emphasis: {
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    grid: {
+      left: "1%",
+      right: "15%",
+      bottom: "0%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "category",
+      data: data.map((d) => d.field),
+      axisLabel: {
+        interval: 0,
+        rotate: 30,
+        color: "white",
+        formatter: function (value: string) {
+          return value.length > 10 ? value.slice(0, 10) + "…" : value;
+        },
+      },
+      name: "Valdkond",
+      nameTextStyle: {
+        color: "white",
+      },
+    },
+    yAxis: {
+      type: "value",
+      name: "Alustajate arv",
+      axisLabel: {
+        color: "white",
+      },
+      nameTextStyle: {
+        color: "white",
+      },
+    },
+    series: [
+      {
+        name: "Alustajate arv",
+        type: "bar",
+        data: data.map((d) => d.rate),
         itemStyle: {
-          color: '#2f4554'
-        }
-      }
-    }
-  ]
-};
+          color: "#5470C6",
+        },
+        emphasis: {
+          itemStyle: {
+            color: "#2f4554",
+          },
+        },
+      },
+    ],
+  };
 
-
-return (
-  <div className="w-full">
-    <ReactEcharts option={option} />
-  </div>
-);   
+  return (
+    <div className="w-full">
+      <ReactEcharts option={option} />
+    </div>
+  );
 }
