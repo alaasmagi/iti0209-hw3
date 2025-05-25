48600ca6846b0c3de263a8ae6ae390ec815166c1
Aleksander Laasmägi <allaas@taltech.ee>
2025-05-25 20:25:40 +0300

    Bunch of changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import IctPopularityPerYearChart from "./components/IctPopularityPerYearChart";
 function App() {
   return (
     <div className="flex flex-col gap-5">
-      <h1 className="text-2xl text-white font-bold">Hariduslikud andmed</h1>
+      <h1 className="text-3xl text-white font-bold pt-2 pb-10">Hariduslikud andmed</h1>
       <div className="flex flex-row gap-5">
         <AcceptedPerYearChart />
         <GraduatesPerYearChart />
